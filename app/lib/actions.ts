3e56a9753838fdab08c529a3bc659c0c089b64a5
Ercilia Maria <erciliacarvalho2@gmail.com>
2025-09-03 16:22:47 -0300

    Update Invoice pelo action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -48,3 +48,29 @@ export async function createInvoice(formData: FormData) {
     //direciona o usuário pra esse arquivo apos criar nova fatura
      redirect('/dashboard/invoices');
 }
+
+/*
+Update Invoice
+*/
+
+//validando os dados passados
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
