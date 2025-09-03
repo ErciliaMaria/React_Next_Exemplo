fe2562cf776dfad1ad424990bf8f90d45d365bf9
Ercilia Maria <erciliacarvalho2@gmail.com>
2025-09-03 15:28:37 -0300

    Criando Invoice pelo action

---
+++ app/lib/actions.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+//validation
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+
+    /**
+     * Dica: Se você estiver trabalhando com formulários
+     *  que possuem muitos campos, você pode considerar usar
+     *  o entries() método com JavaScriptObject.fromEntries().
+     */
+
+    const {customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+   await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  /**
+   * Depois que o banco de dados for atualizado, o 
+   * /dashboard/invoicescaminho será revalidado e novos
+   *  dados serão buscados do servidor.
+   */
+    revalidatePath('/dashboard/invoices');
+
+    //direciona o usuário pra esse arquivo apos criar nova fatura
+     redirect('/dashboard/invoices');
+}
